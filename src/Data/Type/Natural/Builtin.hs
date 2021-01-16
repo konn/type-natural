80397393f43cac8a019dbc27ce9e73ddd2631fda
Hiromi Ishii <konn.jinro@gmail.com>
2021-01-17 04:31:46 +0900

    NoStarIsType

--- src/Data/Type/Natural/Builtin.hs
+++ src/Data/Type/Natural/Builtin.hs
@@ -1,21 +1,3 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE EmptyCase #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE NoStarIsType #-}
-
 -- | Since 1.0.0.0
 module Data.Type.Natural.Builtin
   {-# DEPRECATED "Use Data.Type.Natural instead" #-}
